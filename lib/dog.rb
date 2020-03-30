90aebd4613ec07ce7f743ad95668b48070b1e70c
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-30 22:53:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,12 +70,12 @@ class Dog
 
  def self.find_or_create_by(name:, breed:)
   sql = <<-SQL
-  SELECT *
-  FROM dogs
-  WHERE name = ?
-  AND breed = ?
-  LIMIT 1
-  SQL
+        SELECT *
+        FROM dogs
+        WHERE name = ?
+        AND breed = ?
+        LIMIT 1
+        SQL
 
    dog = DB[:conn].execute(sql, name, breed)
 
