fefd0ac74a173c9162622f6ddeb2c917e2bd14b9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-30 22:52:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,8 +77,8 @@ class Dog
   LIMIT 1
   SQL
 
-   dog =
-   DB[:conn].execute(sql, name, breed)
+   dog = DB[:conn].execute(sql, name, breed)
+
    if !dog.empty?
      dog_data = dog[0]
      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
