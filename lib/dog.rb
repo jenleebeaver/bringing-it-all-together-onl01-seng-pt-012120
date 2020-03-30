3335467e3b2c7d2edd0d940e4ffed4cb5f9182f1
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-30 22:28:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,8 +38,11 @@ class Dog
         DB[:conn].execute(sql, self.name, self.breed)
 
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-  end
+      end
   self
-end
+ end
+
+ def self.create
+ end
 
 end
